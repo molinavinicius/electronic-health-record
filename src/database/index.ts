51bbb971480846a85d1ff2713e173cd2f6694390
molinavinicius <vinymg98@gmail.com>
2023-07-30 14:37:04 -0300

    chore: scripts for migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "../models/user"
-import { Patient } from "../models/patient"
-import { Reservation } from "../models/reservation"
-import { HealthRecord } from "../models/healthRecord"
+
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
@@ -11,7 +8,6 @@ export const AppDataSource = new DataSource({
     synchronize: true,
     logging: false,
     entities: ["src/models/**/*.ts"],
-    migrationsTableName: "ehr_migration_table",
-    migrations: ["src/database/migrations/**/*.ts"],
-    subscribers: [],
+    migrations: ["src/database/migrations/**/*{.ts,.js}"],
+    subscribers: []
 })
